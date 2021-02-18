562c053d1e153eb304db646e2e41b88c175b5598
Richelle Soares <65849468+ricnow@users.noreply.github.com>
2021-02-18 16:52:44 -0300

    Update PrimeiroDesafio.cs

--- Desafio01/PrimeiroDesafio.cs
+++ Desafio01/PrimeiroDesafio.cs
@@ -1,13 +1,40 @@
 using System;
 
-namespace Quartodesafio
+namespace prova_gft
 {
-    public class Caminhao : Transporte
+    //Na hora de Enviar o meu projeto no GIT, foi o arquivo errado do primeiro desafio
+    class PrimeiroDesafio
     {
-        public Caminhao(double peso, double valor, double carga);
+        static void (string[] args)
+        {
+            const int contador = 3;
+            int[] numero = new int[contador];
+            int menor = 0, maior=0;
+            Console.WriteLine("Digite o Três Números: ");
 
-        public void calcularFrete(){
-            
+            for(int i = 0; i < contador; i++)
+            {
+                Console.WriteLine( ( i + 1 ) + "º número:" );
+                numero[i] = Convert.ToInt32( Console.ReadLine() );
+
+                if( i == 0 )
+                    {
+                        menor = numero[0];
+                        maior = numero[0];
+                    }
+
+                    if( numero[i] < menor )
+                    {
+
+                        menor = numero[i];
+                    }
+                    else if( numero[i] > maior )
+                    {
+                        maior = numero[i];
+                    }
+            }
+            Console.WriteLine( "O menor número é:" + menor );
+            Console.WriteLine( "O maior número é:" + maior );
         }
-    }
+    }        
 }
