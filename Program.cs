4ee89aa28b89469c869fc0bbb62b5a20e5312e46
Richelle Soares <richelle.soares@gmail.com>
2021-02-18 14:13:03 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prova_gft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
