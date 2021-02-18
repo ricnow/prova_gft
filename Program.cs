ef2fdafa047f88ddb23e726972680a1f9db5d104
Richelle Soares <richelle.soares@gmail.com>
2021-02-18 14:39:18 -0300

    Envio d Primeiro desafio

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace prova_gft
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int contador = 3;
+            int[] numero = new int[contador];
+            int menor = 0, maior=0;
+            Console.WriteLine("Digite o Três Números: ");
+
+            for(int i = 0; i < contador; i++)
+            {
+                Console.WriteLine( ( i + 1 ) + "º número:" );
+                numero[i] = Convert.ToInt32( Console.ReadLine() );
+
+                if( i == 0 )
+                    {
+                        menor = numero[0];
+                        maior = numero[0];
+                    }
+
+                    if( numero[i] < menor )
+                    {
+
+                        menor = numero[i];
+                    }
+                    else if( numero[i] > maior )
+                    {
+                        maior = numero[i];
+                    }
+            }
+            Console.WriteLine( "O menor número é:" + menor );
+            Console.WriteLine( "O maior número é:" + maior );
         }
-    }
+    }        
 }
